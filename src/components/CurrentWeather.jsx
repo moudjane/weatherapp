b50b59c505c1665de974ce8045dc29599c2868a6
moudjane <mathis.oudjane@gmail.com>
2022-12-17 14:53:57 +0100

    current weather

---
+++ src/components/CurrentWeather.jsx
@@ -0,0 +1,108 @@
+import './CurrentWeather.css';
+import { useState } from 'react';
+function CurrentWeather() {
+
+    const [city, setCity] = useState('')
+    const [weather, setWeather] = useState('')
+    const [backgroundImage, setBackgroundImage] = useState('https://media.tenor.com/ffN-es3aN5cAAAAC/peepokc-kcpeepo.gif')
+    //https://media.tenor.com/ffN-es3aN5cAAAAC/peepokc-kcpeepo.gif
+
+    function weatherz() {
+        fetch('  https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=93f353be3ea3bf5d4140db74968c1fa8&units=metric')
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                setWeather(data)
+                if (data.weather[0].main === "Clouds") {
+                    setBackgroundImage('https://p4.wallpaperbetter.com/wallpaper/981/359/800/blue-clouds-cloudy-hd-wallpaper-preview.jpg')
+                }
+                else if (data.weather[0].main === "Rain") {
+                    setBackgroundImage('https://risibank.fr/cache/medias/0/5/578/57856/full.gif')
+                }
+                else if (data.weather[0].main === "Clear") {
+                    setBackgroundImage('https://pbs.twimg.com/media/FTgR2LgXsAEzmx2.jpg')
+                }
+                else if (data.weather[0].main === "Drizzle") {
+                    setBackgroundImage('https://media.tenor.com/1f0XJ3jKROkAAAAM/pepe-apu-rain-raincoat-for-profile-picture.gif')
+                }
+                else if (data.weather[0].main === "Snow") {
+                    setBackgroundImage('https://media.tenor.com/mCJAwcc1ZbMAAAAC/pepe-christmas.gif')
+                }
+                else if (data.weather[0].main === "ThunderStorm") {
+                    setBackgroundImage('https://media.tenor.com/1f0XJ3jKROkAAAAM/pepe-apu-rain-raincoat-for-profile-picture.gif')
+                }
+                else if (data.weather[0].main === "Mist") {
+                    setBackgroundImage('https://media.tenor.com/1f0XJ3jKROkAAAAM/pepe-apu-rain-raincoat-for-profile-picture.gif')
+                }
+                else if (data.weather[0].main === "Haze") {
+                    setBackgroundImage('https://media.tenor.com/1f0XJ3jKROkAAAAM/pepe-apu-rain-raincoat-for-profile-picture.gif')
+                }
+                else if (data.weather[0].main === "Fog") {
+                    setBackgroundImage('https://media.tenor.com/1f0XJ3jKROkAAAAM/pepe-apu-rain-raincoat-for-profile-picture.gif')
+                }
+                else if (data.weather[0].main === "") {
+                    setBackgroundImage('https://media.tenor.com/ffN-es3aN5cAAAAC/peepokc-kcpeepo.gif')
+                }
+                console.log(data.weather[0].main)
+                console.log(data)
+            })
+    }
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            weatherz(city);
+        }
+    };
+    return (
+
+        <div className='App'>
+            {/* <h1>WEATHEEEEEEEEER</h1> */}
+            <div className="bg" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+
+            <div className='input'>
+                <label>Your city: </label>
+
+                <input type="text" placeholder="Type a city" value={city} onChange={e => setCity(e.target.value)} onKeyDown={handleKeyDown}></input>
+
+                {/* <button onClick={weatherz}>LETSSSGO</button> */}
+
+            </div>
+
+            {weather.main ?
+                <div className='weather-mainz'>
+                    <div className='weather-main2'>
+                        <p className='city'>{weather.name}</p>
+                    </div>
+                </div>
+                : null}
+            {weather.main ?
+                <div className='weather-main'>
+
+                    <div className='weather-main2'>
+
+                        <p className='temp'>{weather.main.temp}°C</p>
+                    </div>
+                </div>
+                : null}
+            {/* {weather.main ?
+                <div className='weather-icon'>
+                    <img src={'https://openweathermap.org/img/wn/' + weather.weather[0].icon + '@4x.png'} />
+                </div>
+                : null} */}
+            {weather.main ?
+                <div className='weather-global'>
+                    <div className='bloc1'>Humidity:
+                        <div className='weather-stat'>{weather.main.humidity} %</div>
+                    </div>
+                    <div className='bloc2'>Wind speed:
+                        <div className='weather-stat'>{weather.wind.speed} m/s</div>
+                    </div>
+                    <div className='bloc3'>Feels like:
+                        <div className='weather-stat'>{weather.main.feels_like} °C</div>
+                    </div>
+                </div>
+                : null}
+
+        </div >
+    )
+}
+export default CurrentWeather;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonList } from '@ionic/react';
+import CurrentWeather from '../components/CurrentWeather';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      {/* <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Current Weather</IonTitle>
         </IonToolbar>
-      </IonHeader>
+      </IonHeader> */}
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Current Weather</IonTitle>
           </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        </IonHeader> */}
+
+        <CurrentWeather />
+
+        {/* <IonItem>
+          <IonLabel>Your city:</IonLabel>
+          <IonInput placeholder="Enter city name"></IonInput>
+        </IonItem> */}
       </IonContent>
     </IonPage>
   );
